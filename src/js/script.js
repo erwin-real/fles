d6b57eb21dc6e9e2b4d516ad3c4fd92cf3e0c157
erwin-real <erwinsantoscapati@gmail.com>
2019-11-29 14:21:28 +0800

    added clear

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,18 @@
 $(document).ready(function () {
 
     $("#maxItemsBtn").click(function () {
+        $('.items-holder').empty();
+
         for (var i = 1; i <= $("#maxItems").val(); i++)
             $('.items-holder').append('<label>' + i + '.)</label> <textarea name="items[]" cols="80" rows="1"></textarea><br />');
     });
 
+    $("#clrMaxItemsBtn").click(function () { $('.items-holder').empty(); });
+    $("#clrMaxStudentsBtn").click(function () { $('.scores-holder').empty(); });
+
     $("#maxStudentsBtn").click(function () {
+        $('.scores-holder').empty();
+
         for (var i = 1; i <= $("#maxStudents").val(); i++)
             $('.scores-holder').append('<label>Student ' + i + ':</label> <input type="number" name"scores[]" placeholder="Score of student ' + i + '"><br />');
 
